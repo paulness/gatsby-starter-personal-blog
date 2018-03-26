75979f02bf25d8c68f80bafb416c9a2067f39d73
paulness <admin@paulsness.com>
2018-03-26 08:37:53 -0400

    Updates

---
+++ path---index-ec9db78204994f2f21a6.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{827:function(t,a){t.exports={data:{site:{siteMetadata:{facebook:{appId:"445996885830237"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-ec9db78204994f2f21a6.js.map
