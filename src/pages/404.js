7df67fa49a8b00980035903e7128e24861c19314
paulness <admin@paulsness.com>
2018-04-01 18:47:55 -0400

    Add basic 404 not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Main from "../components/Main";
 import Article from "../components/Main/Article";
 import PageHeader from "../components/Page/PageHeader";
+import Content from "../components/Main/Content";
 
 const NotFoundPage = props => {
     const { data } = props;
@@ -12,7 +13,7 @@ const NotFoundPage = props => {
       <Main>
         <Article>
           <PageHeader title="404 Not Found" />
-          Sorry the content, you were looking for is not avaliable.
+          <Content html='Sorry the content, you were looking for is not avaliable.' />
         </Article>
       </Main>
     );
